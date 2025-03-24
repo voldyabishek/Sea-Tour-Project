b5ac904593ed078e3d92b6de07fe14b9c2e82c7c
voldyabishek <aa9177954@gmail.com>
2025-03-24 12:18:39 +0530

    add  scrolling animation (img -text) croos animation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-import '../CssComponents/Mainsection.css'
+import '../CssComponents/Home.css'
 import { Link } from 'react-router-dom'
 
 import Services from './Services'
 import Destination from './Destination'
 // JQuaery for back to Top Button Script 
-import "../JScomponent/Backtotop"
+import "../JScomponent/imgCroses.js"
 
 const Home = () => {
 
@@ -21,7 +21,7 @@ const Home = () => {
 
       {/* Section 1 */}
       <section className='section-1'>
-        {/* <img src="MainImg-4.jpg" className='section1img' alt="Main" /> */}
+        {/* <img src="MainImg-4.jpg" classNameName='section1img' alt="Main" /> */}
         <video className='section1video' autoPlay loop muted>
           <source src="MainPage-Video.mp4" type="video/mp4" />
 
@@ -36,14 +36,60 @@ const Home = () => {
           </p>
           <Link to="/Booking"><button className=" book-now" onClick={scrollToBooking}>Book Now</button></Link>
         </div>
-        {/* this div for Back to Top Button  */}
-    {/* <button id="back-to-top" class="back-to-top">
-        🐟
-    </button> */}
+      
 
       </section>
 
       <Services />
+      {/* this div for croos scroling animation with image section */}
+      <div className="skw-pages">
+  <div className="skw-page skw-page-2">
+    <div className="skw-page__half skw-page__half--left">
+      <div className="skw-page__skewed">
+        <div className="skw-page__content">
+          <h2 className="skw-page__heading">Page 2</h2>
+          <p className="skw-page__description">Nothing to do here, continue scrolling.</p>
+        </div>
+      </div>
+    </div>
+    <div className="skw-page__half skw-page__half--right">
+      <div className="skw-page__skewed">
+        <div className="skw-page__content">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="skw-page skw-page-3">
+    <div className="skw-page__half skw-page__half--left">
+      <div className="skw-page__skewed">
+        <div className="skw-page__content"></div>
+      </div>
+    </div>
+    <div className="skw-page__half skw-page__half--right">
+      <div className="skw-page__skewed">
+        <div className="skw-page__content">
+          <h2 className="skw-page__heading">Page 3</h2>
+          <p className="skw-page__description">The end is near, I promise!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="skw-page skw-page-4">
+    <div className="skw-page__half skw-page__half--left">
+      <div className="skw-page__skewed">
+        <div className="skw-page__content">
+          <h2 className="skw-page__heading">Page 4</h2>
+          <p className="skw-page__description">Ok, ok, just one more scroll!</p>
+        </div>
+      </div>
+    </div>
+    <div className="skw-page__half skw-page__half--right">
+      <div className="skw-page__skewed">
+        <div className="skw-page__content"></div>
+      </div>
+    </div>
+  </div>
+</div>
       <Destination />
  
     </>
